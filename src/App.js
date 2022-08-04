86ce8a7e695feb8db44c1f2df7d02897b73e93f8
yayner <yayner2002@gmail.com>
2022-08-04 09:50:06 +0300

    Change the components into functional comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
