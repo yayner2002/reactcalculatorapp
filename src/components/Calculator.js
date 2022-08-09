471b99c5002affd7232c948e01fd766fe33b08d8
yayner <yayner2002@gmail.com>
2022-08-09 11:08:38 +0300

    Finish the routing functinality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,79 +15,81 @@ function Calculator() {
     setInitialState((prevState) => calculate(prevState, buttonName));
   };
   return (
-    <div className="container">
-      <div className="result-container">
-        <span>{total}</span>
-        <span>{operation}</span>
-        <span>{next}</span>
-      </div>
-      <div className="rows">
-        <div className="row-1">
-          <button type="button" id="AC" onClick={handleClick}>
-            AC
-          </button>
-          <button type="button" id="+/-" onClick={handleClick}>
-            +/-
-          </button>
-          <button type="button" id="%" onClick={handleClick}>
-            %
-          </button>
-          <button type="button" id="÷" onClick={handleClick}>
-            ÷
-          </button>
+    <div className="calculator-container">
+      <div className="container">
+        <div className="result-container">
+          <span>{total}</span>
+          <span>{operation}</span>
+          <span>{next}</span>
         </div>
-        <div className="row-2">
-          <button type="button" id="7" onClick={handleClick}>
-            7
-          </button>
-          <button type="button" id="8" onClick={handleClick}>
-            8
-          </button>
-          <button type="button" id="9" onClick={handleClick}>
-            9
-          </button>
-          <button type="button" id="x" onClick={handleClick}>
-            x
-          </button>
-        </div>
-        <div className="row-3">
-          <button type="button" id="4" onClick={handleClick}>
-            4
-          </button>
-          <button type="button" id="5" onClick={handleClick}>
-            5
-          </button>
-          <button type="button" id="6" onClick={handleClick}>
-            6
-          </button>
-          <button type="button" id="-" onClick={handleClick}>
-            -
-          </button>
-        </div>
-        <div className="row-4">
-          <button type="button" id="1" onClick={handleClick}>
-            1
-          </button>
-          <button type="button" id="2" onClick={handleClick}>
-            2
-          </button>
-          <button type="button" id="3" onClick={handleClick}>
-            3
-          </button>
-          <button type="button" id="+" onClick={handleClick}>
-            +
-          </button>
-        </div>
-        <div className="row-5">
-          <button type="button" id="0" onClick={handleClick}>
-            0
-          </button>
-          <button type="button" id="." onClick={handleClick}>
-            .
-          </button>
-          <button type="button" id="=" onClick={handleClick}>
-            =
-          </button>
+        <div className="rows">
+          <div className="row-1">
+            <button type="button" id="AC" onClick={handleClick}>
+              AC
+            </button>
+            <button type="button" id="+/-" onClick={handleClick}>
+              +/-
+            </button>
+            <button type="button" id="%" onClick={handleClick}>
+              %
+            </button>
+            <button type="button" id="÷" onClick={handleClick}>
+              ÷
+            </button>
+          </div>
+          <div className="row-2">
+            <button type="button" id="7" onClick={handleClick}>
+              7
+            </button>
+            <button type="button" id="8" onClick={handleClick}>
+              8
+            </button>
+            <button type="button" id="9" onClick={handleClick}>
+              9
+            </button>
+            <button type="button" id="x" onClick={handleClick}>
+              x
+            </button>
+          </div>
+          <div className="row-3">
+            <button type="button" id="4" onClick={handleClick}>
+              4
+            </button>
+            <button type="button" id="5" onClick={handleClick}>
+              5
+            </button>
+            <button type="button" id="6" onClick={handleClick}>
+              6
+            </button>
+            <button type="button" id="-" onClick={handleClick}>
+              -
+            </button>
+          </div>
+          <div className="row-4">
+            <button type="button" id="1" onClick={handleClick}>
+              1
+            </button>
+            <button type="button" id="2" onClick={handleClick}>
+              2
+            </button>
+            <button type="button" id="3" onClick={handleClick}>
+              3
+            </button>
+            <button type="button" id="+" onClick={handleClick}>
+              +
+            </button>
+          </div>
+          <div className="row-5">
+            <button type="button" id="0" onClick={handleClick}>
+              0
+            </button>
+            <button type="button" id="." onClick={handleClick}>
+              .
+            </button>
+            <button type="button" id="=" onClick={handleClick}>
+              =
+            </button>
+          </div>
         </div>
       </div>
     </div>
