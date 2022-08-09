4a4c1b9f833f78985c149438be5418c7d87fe5cd
yayner <yayner2002@gmail.com>
2022-08-09 09:45:52 +0300

    Add the pages components

---
+++ src/components/pages/Calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Calculator() {
+  return (
+    <div>
+      <h1>Calculator</h1>
+    </div>
+  );
+}
+
+export default Calculator;
---
+++ src/components/pages/Quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function quote() {
+  return (
+    <div />
+  );
+}
+
+export default quote;
