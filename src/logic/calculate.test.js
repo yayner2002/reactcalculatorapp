3002446554f53c95d614d7d366618b7d5ed60cd9
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-10 10:09:53 +0100

    Add Operate and Calculate tests

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,32 @@
+import calculate from "./calculate";
+
+// arrange
+const mockState = {
+  total: null,
+  next: null,
+  operation: null,
+};
+
+describe("CALCULATE TESTS\n", () => {
+  test("should reset all state (AC)", () => {
+    expect(calculate(mockState, "AC")).toStrictEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+  test("should update number", () => {
+    expect(calculate(mockState, "1")).toStrictEqual({
+      total: null,
+      next: "1"
+    });
+  });
+  test("should add operation", () => {
+    const numState = {...mockState, next : '1'}
+    expect(calculate(numState, "+")).toStrictEqual({
+      operation: "+",
+      total: "1",
+      next: null
+    });
+  });
+});
